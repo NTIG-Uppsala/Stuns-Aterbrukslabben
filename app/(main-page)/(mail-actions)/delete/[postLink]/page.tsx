5081e38f4ea4847babedf78974b5bdd3f07f3a57
Simon Clavensjö <simon@clavensjo.se>
2024-04-08 14:00:38 +0200

    Update delete post by mail error message

--- app/(main-page)/(mail-actions)/delete/[postLink]/page.tsx
+++ app/(main-page)/(mail-actions)/delete/[postLink]/page.tsx
@@ -14,7 +14,25 @@ export default async function DeletePostByMailPage({ params }: PageProps) {
   });
 
   if (!soonExpiringPost) {
-    return <div>Inget inlägg hittades</div>;
+    return (
+      <div>
+        <div className="flex w-full h-[48vh] items-end justify-center text-center">
+          <div className="flex flex-col max-w-screen-sm gap-y-2 px-3">
+            <h1 className="text-xl font-medium">Inget inlägg hittades</h1>
+            <p className="text-balance">
+              Ojdå, något gick fel och ingen post hittades. Var god och kontakta
+              oss om problemet kvarstår.
+            </p>
+            <Link
+              className="text-blue-600 hover:underline pt-1 text-lg"
+              href="/"
+            >
+              Till startsidan
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   const response = await deletePostsByIds({ postsIds: [soonExpiringPost.id] });
