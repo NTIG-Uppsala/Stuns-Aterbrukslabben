48eaf82eaf422a42563b74dd23773cb81783ba29
lukasgurra <142992155+lukasgurra@users.noreply.github.com>
2024-04-09 15:56:00 +0200

    Add terms of service page

--- app/(main-page)/_components/footer.tsx
+++ app/(main-page)/_components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="flex w-full mt-20 bg-gradient-to-b from-secondary to-navbarStart">
+    <footer className="flex w-full md:mt-16 mt-12 bg-gradient-to-b from-secondary to-navbarStart">
       <div className="flex justify-around max-w-[1000px] py-10 mx-auto items-center w-full">
         <Logo />
         <Link
