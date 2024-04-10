810c65c92d14b743a90c5ea6dbc29e593352d16a
Simon Clavensjö <simon@clavensjo.se>
2024-04-10 14:35:43 +0200

    Redesign contact disclaimer

--- app/(main-page)/post/_components/contact-me-dialog.tsx
+++ app/(main-page)/post/_components/contact-me-dialog.tsx
@@ -22,42 +22,47 @@ export default function ContactMeDialog({ name, email }: ContactMeDialogProps) {
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <p className="md:text-base text-sm text-start">
-            När du skänker eller tar emot begagnad utrustning, vänligen tänk på
-            följande:
-          </p>
-          <ul className="flex flex-col gap-y-1 list-disc md:text-sm text-xs md:px-5 text-start">
-            <li>
+          <h1 className="md:text-lg text-sm text-center text-pretty">
+            När du skänker eller tar emot begagnad utrustning, tänk på följande
+          </h1>
+          <div className="flex flex-col gap-y-2 list-disc md:text-sm text-xs md:px-5 text-center text-balance items-center">
+            <hr />
+            <p>
               Säkerställ så att utrustningen är i gott skick och fungerar
               korrekt. Defekt utrustning kan utgöra en säkerhetsrisk för
               användaren.
-            </li>
-            <li>
+            </p>
+            <hr className="w-4/5" />
+            <p>
               Viss utrustning kan behöva regelbundet underhåll för att fortsätta
               fungera korrekt och säkert. Regelbundna kalibreringar kan vara
               nödvändiga.
-            </li>
-            <li>
+            </p>
+            <hr className="w-4/5" />
+            <p>
               Säkerställ så att utrustning och material rengjorts från
               hälsovådliga ämnen, kemikalier och biologiskt material som
               utrustningen tidigare varit i kontakt med. Tänk särskilt på att
               utrustning som använts i forskning kan ha kontaminerats med ämnen
               vars hälsorisker ännu inte är kända.
-            </li>
-            <li>
+            </p>
+            <hr className="w-4/5" />
+            <p>
               Vid nyttjande av begagnad utrustning är det användaren som bär
               ansvar för den egna säkerheten. Återbrukslabbet förmedlar endast
               kontakt mellan den som skänker, respektive tar emot utrustning och
               bär därför inget ansvar för utrustningens skick eller säkerhet.
-            </li>
-          </ul>
+            </p>
+          </div>
           <p className="flex justify-center text-lg md:pt-6 pt-2">{name}</p>
-          <a
-            className="flex justify-center hover:underline text-blue-600"
-            href={`mailto:${email}`}
-          >
-            {email}
-          </a>
+          <div className="flex justify-center">
+            <a
+              className="flex w-fit justify-center hover:underline text-blue-600"
+              href={`mailto:${email}`}
+            >
+              {email}
+            </a>
+          </div>
         </DialogHeader>
       </DialogContent>
     </Dialog>
