73abef9c544aaec47e74b7f5d0add26acdcc85fc
Simon Clavensjö <simon@clavensjo.se>
2024-04-15 10:42:14 +0200

    remove unnecessary imports

--- app/api/_utils/send-mail-to-expiring-posts.ts
+++ app/api/_utils/send-mail-to-expiring-posts.ts
@@ -1,6 +1,5 @@
-import { clerkClient } from "@clerk/nextjs";
 import getNameAndEmailFromUserId from "@/utils/get-name-and-email-from-user-id";
-import getUserEmail from "@/utils/get-user-email";
+
 import { type Post } from "@prisma/client";
 import sendMail from "@/utils/send-mail";
 
