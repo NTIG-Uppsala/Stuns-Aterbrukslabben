37709a6d85004e4e93c16b8975928c74b0b80366
lukasgurra <142992155+lukasgurra@users.noreply.github.com>
2024-04-18 14:31:42 +0200

    Add disclaimer on create post alert dialog
    Co-Authored-By: Simon Clavensjö <142975808+SimonClavensjo@users.noreply.github.com>

--- app/(main-page)/create-post/_components/create-alert-dialog.tsx
+++ app/(main-page)/create-post/_components/create-alert-dialog.tsx
@@ -28,10 +28,40 @@ export default function CreateAlertDialog({
         <AlertDialogHeader>
           <AlertDialogTitle>Skapa inlägg?</AlertDialogTitle>
           <AlertDialogDescription>
-            <p className="md:text-sm text-xs text-center text-balance">
-              Säkerställ att utrustningen är i gott skick och fungerar korrekt.
-              Defekt utrustning kan utgöra en säkerhetsrisk för användaren.
-            </p>
+            <h1 className="md:text-base text-sm text-center text-pretty">
+              När du skänker eller tar emot begagnad utrustning, tänk på
+              följande
+            </h1>
+            <div className="flex flex-col gap-y-2 list-disc md:text-sm text-xs md:px-5 text-center text-pretty items-center">
+              <hr />
+              <p>
+                Säkerställ så att utrustningen är i gott skick och fungerar
+                korrekt. Defekt utrustning kan utgöra en säkerhetsrisk för
+                användaren.
+              </p>
+              <hr className="w-4/5" />
+              <p>
+                Viss utrustning kan behöva regelbundet underhåll för att
+                fortsätta fungera korrekt och säkert. Regelbundna kalibreringar
+                kan vara nödvändiga.
+              </p>
+              <hr className="w-4/5" />
+              <p>
+                Säkerställ så att utrustning och material rengjorts från
+                hälsovådliga ämnen, kemikalier och biologiskt material som
+                utrustningen tidigare varit i kontakt med. Tänk särskilt på att
+                utrustning som använts i forskning kan ha kontaminerats med
+                ämnen vars hälsorisker ännu inte är kända.
+              </p>
+              <hr className="w-4/5" />
+              <p>
+                Vid nyttjande av begagnad utrustning är det användaren som bär
+                ansvar för den egna säkerheten. Återbrukslabbet förmedlar endast
+                kontakt mellan den som skänker, respektive tar emot utrustning
+                och bär därför inget ansvar för utrustningens skick eller
+                säkerhet.
+              </p>
+            </div>
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
