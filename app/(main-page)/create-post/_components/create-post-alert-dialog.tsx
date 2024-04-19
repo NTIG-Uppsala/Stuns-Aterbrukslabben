c218f93a6cd36a0bfb92b61e3a8b4dc8912c475f
lukasgurra <142992155+lukasgurra@users.noreply.github.com>
2024-04-19 12:57:41 +0200

    Fix nested p tags in alert dialog description

--- app/(main-page)/create-post/_components/create-post-alert-dialog.tsx
+++ app/(main-page)/create-post/_components/create-post-alert-dialog.tsx
@@ -5,7 +5,6 @@ import {
   AlertDialogAction,
   AlertDialogCancel,
   AlertDialogContent,
-  AlertDialogDescription,
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
@@ -27,42 +26,38 @@ export default function CreatePostAlertDialog({
       <AlertDialogContent>
         <AlertDialogHeader>
           <AlertDialogTitle>Skapa inlägg?</AlertDialogTitle>
-          <AlertDialogDescription>
-            <h1 className="md:text-base text-sm text-center text-pretty">
-              När du skänker eller tar emot begagnad utrustning, tänk på
-              följande
-            </h1>
-            <div className="flex flex-col gap-y-2 list-disc md:text-sm text-xs md:px-5 text-center text-pretty items-center">
-              <hr />
-              <p>
-                Säkerställ så att utrustningen är i gott skick och fungerar
-                korrekt. Defekt utrustning kan utgöra en säkerhetsrisk för
-                användaren.
-              </p>
-              <hr className="w-4/5" />
-              <p>
-                Viss utrustning kan behöva regelbundet underhåll för att
-                fortsätta fungera korrekt och säkert. Regelbundna kalibreringar
-                kan vara nödvändiga.
-              </p>
-              <hr className="w-4/5" />
-              <p>
-                Säkerställ så att utrustning och material rengjorts från
-                hälsovådliga ämnen, kemikalier och biologiskt material som
-                utrustningen tidigare varit i kontakt med. Tänk särskilt på att
-                utrustning som använts i forskning kan ha kontaminerats med
-                ämnen vars hälsorisker ännu inte är kända.
-              </p>
-              <hr className="w-4/5" />
-              <p>
-                Vid nyttjande av begagnad utrustning är det användaren som bär
-                ansvar för den egna säkerheten. Återbrukslabbet förmedlar endast
-                kontakt mellan den som skänker, respektive tar emot utrustning
-                och bär därför inget ansvar för utrustningens skick eller
-                säkerhet.
-              </p>
-            </div>
-          </AlertDialogDescription>
+          <h1 className="md:text-base text-sm text-center text-pretty">
+            När du skänker eller tar emot begagnad utrustning, tänk på följande
+          </h1>
+          <div className="flex flex-col gap-y-2 list-disc md:text-sm text-xs md:px-5 text-center text-pretty items-center">
+            <hr />
+            <p>
+              Säkerställ så att utrustningen är i gott skick och fungerar
+              korrekt. Defekt utrustning kan utgöra en säkerhetsrisk för
+              användaren.
+            </p>
+            <hr className="w-4/5" />
+            <p>
+              Viss utrustning kan behöva regelbundet underhåll för att fortsätta
+              fungera korrekt och säkert. Regelbundna kalibreringar kan vara
+              nödvändiga.
+            </p>
+            <hr className="w-4/5" />
+            <p>
+              Säkerställ så att utrustning och material rengjorts från
+              hälsovådliga ämnen, kemikalier och biologiskt material som
+              utrustningen tidigare varit i kontakt med. Tänk särskilt på att
+              utrustning som använts i forskning kan ha kontaminerats med ämnen
+              vars hälsorisker ännu inte är kända.
+            </p>
+            <hr className="w-4/5" />
+            <p>
+              Vid nyttjande av begagnad utrustning är det användaren som bär
+              ansvar för den egna säkerheten. Återbrukslabbet förmedlar endast
+              kontakt mellan den som skänker, respektive tar emot utrustning och
+              bär därför inget ansvar för utrustningens skick eller säkerhet.
+            </p>
+          </div>
         </AlertDialogHeader>
         <AlertDialogFooter>
           <AlertDialogCancel>Fortsätt redigera</AlertDialogCancel>
