a088b9cc3e238b860d5cc8b22ede23abf9ae7638
Simon Clavensjö <simon@clavensjo.se>
2024-04-24 10:47:36 +0200

    Archive posts on account deletion

--- app/api/_utils/delete-users-posts.ts
+++ app/api/_utils/delete-users-posts.ts
@@ -9,13 +9,39 @@ interface DeleteUsersPostsProps {
 export default async function deleteUsersPosts({
   deletedUsersId,
 }: DeleteUsersPostsProps) {
-  try {
-    await db.post.deleteMany({
+  const [posts] = await db.$transaction([
+    db.post.findMany({
       where: {
         userId: deletedUsersId,
       },
+    }),
+
+    db.post.deleteMany({
+      where: {
+        userId: deletedUsersId,
+      },
+    }),
+  ]);
+
+  try {
+    posts.forEach(async (post) => {
+      await db.archivedPosts.create({
+        data: {
+          id: post.id,
+          deletionReason: "Användare bortagen",
+          archivedAt: new Date(),
+          createdAt: post.createdAt,
+          location: post.location,
+          title: post.title,
+          description: post.description,
+          postType: post.postType,
+          category: post.category,
+          hasCustomExpirationDate: post.hasCustomExpirationDate,
+        },
+      });
     });
   } catch {
-    return { error: "Failed to delete user's posts" };
+    return { error: "Failed to archive user's posts" };
   }
+  return { error: "Failed to delete user's posts" };
 }
