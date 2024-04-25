d6dd8d6c92bb3ac0a11853414714326f43feb531
Simon Clavensjö <simon@clavensjo.se>
2024-04-25 11:35:39 +0200

    Change label to h2

--- app/(main-page)/admin/page.tsx
+++ app/(main-page)/admin/page.tsx
@@ -34,7 +34,7 @@ export default async function AdminDashboard({
   return (
     <div className="max-w-screen-md mx-auto p-3 pt-10">
       <div className="flex flex-col text-center items-center mb-10 p-3 w-full bg-white rounded-md">
-        <label className="text-xl pb-3">Exportera arkiverade inlägg</label>
+        <h2 className="text-xl pb-3">Exportera arkiverade inlägg</h2>
         <ExportArchivesButton />
       </div>
       <SearchBar labelText={labelText} itemsFoundCount={queriedUserCount} />
