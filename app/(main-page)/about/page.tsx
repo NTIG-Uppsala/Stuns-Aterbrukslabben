4854b08d3b7fd5089c10c8e6b10d7c10678b0e5c
Simon Clavensjö <simon@clavensjo.se>
2024-05-02 14:50:06 +0200

    Add simon

--- app/(main-page)/about/page.tsx
+++ app/(main-page)/about/page.tsx
@@ -45,6 +45,23 @@ export default function AboutPage() {
           >
             LinkedIn
           </a>
+          ,{" "}
+          <a
+            className="hover:underline text-blue-600"
+            href="https://github.com/SimonClavensjo"
+          >
+            Github
+          </a>
+          ,{" "}
+          <a
+            className="hover:underline text-blue-600"
+            href="https://simon.clavensjo.se/"
+          >
+            portfolio
+          </a>
+          <br />
+          Planerar att plugga datavetenskap i Stockholm 2024/2027. Intresserad
+          av backend-utveckling.
         </div>
         <div>
           <span className="font-semibold">Lukas Gustafsson</span>,{" "}
@@ -104,8 +121,8 @@ export default function AboutPage() {
           <br />
           Mönstrat och kommit in så kanske gör värnplikten, sökte frivilligt
           vilket betyder att jag kan välja. Jag kollar även vidareutbildningar
-          inom mjukvaruutveckling inriktning frontend och design. Funderar 
-          även på att börja jobba direkt.
+          inom mjukvaruutveckling inriktning frontend och design. Funderar även
+          på att börja jobba direkt.
         </div>
         <div>
           <span className="font-semibold">Ambjörn Hogmark</span>,{" "}
