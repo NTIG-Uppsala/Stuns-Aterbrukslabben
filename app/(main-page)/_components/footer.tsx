f7b6ca92ee7d13db4a83c5e3c6b52dad20307954
lukasgurra <142992155+lukasgurra@users.noreply.github.com>
2024-05-03 09:47:51 +0200

    Remove unnecessary tailwind class

--- app/(main-page)/_components/footer.tsx
+++ app/(main-page)/_components/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="flex w-full md:mt-16 mt-12 bg-gradient-to-b from-secondary to-navbarStart">
+    <footer className="w-full md:mt-16 mt-12 bg-gradient-to-b from-secondary to-navbarStart">
       <div className="flex w-full justify-around max-w-[1000px] md:text-base text-sm font-semibold py-10 mx-auto">
         <Link href="/faq" className="hover:underline">
           Vanliga frågor
